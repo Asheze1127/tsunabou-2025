236c5dd08ec0abe20603d2b2add35c01300aca96
Asheze1127 <m.k.h11272005@gmail.com>
2025-09-11 01:06:17 +0900

    ログインStore実装

--- tsunabou-code/src/App.tsx
+++ tsunabou-code/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
@@ -6,20 +5,29 @@ import Preparedness from './pages/Preparedness';
 import Map from './pages/Map';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
+import { useLoginStore } from './store/login.store';
 
 function App() {
+  const isLoggedIn = useLoginStore((state) => state.isLoggedIn);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/preparedness" element={<Preparedness />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      {isLoggedIn ? (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/preparedness" element={<Preparedness />} />
+              <Route path="/map" element={<Map />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route>
+          </Routes>
+        </Router>
+      ) : (
+        <h1>Login</h1>
+      )}
+    </>
   );
 }
 
